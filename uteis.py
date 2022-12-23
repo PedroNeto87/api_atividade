be8e39b4906f748f592f29896845f663d1634ebd
Pedro Neto <pedrofcn01@hotmail.com>
2022-12-23 17:01:38 -0300

    Alterada nossa REST API para realizar autenticação no banco de dados. Em models, foi criada tabela de usuários para autenticação nas APIs. Incluido em requirements a extensão flask-httpauth.

--- uteis.py
+++ uteis.py
@@ -1,32 +1,45 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 
-def insere_pessoas():
+def inserir_pessoas():
     pessoa = Pessoas(nome='João', idade=30)
     print(pessoa)
     pessoa.save()
 
 
-def consulta_pessoas():
+def consultar_pessoas():
     pessoas = Pessoas.query.all()
     print(pessoas)
-    # pessoa = Pessoas.query.filter_by(nome='João').first()
-    # print(pessoa.idade)
+    pessoa = Pessoas.query.filter_by(nome='João').first()
+    print(pessoa.idade)
 
 
 def alterar_pessoas():
-    pessoa = Pessoas.query.filter_by(nome='Ashia').first()
-    pessoa.nome = 'Marcela'
+    pessoa = Pessoas.query.filter_by(nome='Marcela').first()
+    pessoa.nome = 'Ashia'
     pessoa.save()
 
 
 def excluir_pessoas():
-    pessoa = Pessoas.query.filter_by(nome='Felipe').first()
+    pessoa = Pessoas.query.filter_by(nome='').first()
     pessoa.delete()
 
 
+def inserir_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+
+def consultar_usuario():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
+
+def excluir_usuario(nome):
+    usuario = Usuarios.query.filter_by().first()
+    usuario.delete()
+
+
 if __name__ == '__main__':
-    #insere_pessoas()
-    #alterar_pessoas()
-    excluir_pessoas()
-    consulta_pessoas()
+    inserir_usuario('pedrin', '198090')
+    consultar_usuario()
